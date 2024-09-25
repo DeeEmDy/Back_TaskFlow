98f168125610b1e8c06e885295b77daae810f43a
DeeEmDy <dy-lanarias@hotmail.comcls>
2024-09-25 15:09:24 -0600

    Fix Secret Key to check signature jwt token in jwt.io

--- src/main/java/com/taskflow/backend/config/UserAuthProvider.java
+++ src/main/java/com/taskflow/backend/config/UserAuthProvider.java
@@ -1,6 +1,5 @@
 package com.taskflow.backend.config;
 
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
@@ -20,15 +19,14 @@ import com.taskflow.backend.dto.UserDto;
 import com.taskflow.backend.exception.JwtAuthenticationException;
 import com.taskflow.backend.services.UserService;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-
 
 @RequiredArgsConstructor
 @Component
 public class UserAuthProvider {
 
-    @Value("${security.jwt.token.secret-key:secret-value}")
+    // Cambia la clave secreta aquí
+    @Value("${security.jwt.token.secret-key:your-256-bit-secret}")
     private String secretKey;
 
     @Value("${security.jwt.token.expiration:3600000}") // 1 hora en milisegundos
@@ -37,33 +35,28 @@ public class UserAuthProvider {
     private final UserService userService;
 
     // Set para almacenar tokens revocados
-    private final Set<String> revokedTokens = new HashSet<>(); // Asegúrate de que esto esté aquí
-
-    @PostConstruct
-    protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes()); // Encriptar la clave secreta en base64
-    }
+    private final Set<String> revokedTokens = new HashSet<>(); 
 
     // Método para crear un token JWT
     public String createToken(String email) {
         Date now = new Date();
         Date expiresAt = new Date(now.getTime() + expirationTime);
         return JWT.create()
-                .withSubject(email)  // Utilizamos el email como subject para el token.
+                .withSubject(email) 
                 .withIssuedAt(now)
                 .withExpiresAt(expiresAt)
-                .sign(Algorithm.HMAC256(secretKey));
-    }    
+                .sign(Algorithm.HMAC256(secretKey)); 
+    }
 
     // Método para validar el token JWT
     public Authentication validateToken(String token) {
-        if (revokedTokens.contains(token)) { // Asegúrate de que esto funcione correctamente
+        if (isTokenRevoked(token)) { 
             throw new JwtAuthenticationException("Token has been revoked");
         }
         try {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
             DecodedJWT decodedJWT = verifier.verify(token);
-            UserDto user = userService.findByEmail(decodedJWT.getSubject()); // Cambiado `getIssuer()` a `getSubject()`
+            UserDto user = userService.findByEmail(decodedJWT.getSubject());
             return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
         } catch (JWTVerificationException | IllegalArgumentException e) {
             throw new JwtAuthenticationException("Token expirado o inválido.", e);
@@ -73,24 +66,23 @@ public class UserAuthProvider {
     // Método para crear un token de refrescamiento
     public String createRefreshToken(String email) {
         Date now = new Date();
-        Date expiresAt = new Date(now.getTime() + expirationTime * 2); // Token de refresco tiene una duración más larga
+        Date expiresAt = new Date(now.getTime() + expirationTime * 2); 
         return JWT.create()
-                .withSubject(email) // Cambiar `withIssuer` a `withSubject`
+                .withSubject(email)
                 .withIssuedAt(now)
                 .withExpiresAt(expiresAt)
                 .sign(Algorithm.HMAC256(secretKey));
     }
     
-    // Método para validar el token de refrescamiento.
+    // Método para validar el token de refresco
     public Authentication validateRefreshToken(String token) {
-        if (revokedTokens.contains(token)) {
-            
+        if (isTokenRevoked(token)) {
             throw new JwtAuthenticationException("El token de refresco ha sido revocado.");
         }
         try {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
             DecodedJWT decodedJWT = verifier.verify(token);
-            UserDto user = userService.findByEmail(decodedJWT.getSubject()); // Cambiado `getIssuer()` a `getSubject()`
+            UserDto user = userService.findByEmail(decodedJWT.getSubject());
             return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
         } catch (JWTVerificationException | IllegalArgumentException e) {
             throw new JwtAuthenticationException("Refresh token expirado o inválido.", e);
@@ -99,12 +91,17 @@ public class UserAuthProvider {
     
     // Método para invalidar un token
     public void invalidateToken(String token) {
-        revokedTokens.add(token); //Revocar el token para futuras solicitudes.
+        revokedTokens.add(token); // Revocar el token para futuras solicitudes.
     }
 
-    // Método para obtener el token de autenticación de un objeto Authentication - metodo getTokenFromAuth
+    // Método para obtener el token de autenticación de un objeto Authentication
     public String getTokenFromAuth(Authentication authentication) {
         UserDto user = (UserDto) authentication.getPrincipal();
         return createToken(user.getEmail());
+    }
+
+    // Método auxiliar para verificar si un token ha sido revocado
+    private boolean isTokenRevoked(String token) {
+        return revokedTokens.contains(token);
     }
 }
