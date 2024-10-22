f5cc0c8e049acdbcbc7c3a3d5f14614598b89eb7
DeeEmDy <dy-lanarias@hotmail.comcls>
2024-10-22 15:29:13 -0600

    Add paths in SecurityConfig to CRUD methos in UserController.

--- src/main/java/com/taskflow/backend/config/SecurityConfig.java
+++ src/main/java/com/taskflow/backend/config/SecurityConfig.java
@@ -52,7 +52,9 @@ public class SecurityConfig {
                 .requestMatchers("/user/getAll").hasAnyAuthority("ROLE_ADMIN", "ROLE_NORMUSER")
                 .requestMatchers("/user/create").hasAuthority("ROLE_ADMIN")
                 .requestMatchers("/user/**").hasAuthority("ROLE_NORMUSER")
-                .requestMatchers(HttpMethod.DELETE, "/auth/logout").hasAnyAuthority("ROLE_NORMUSER", "ROLE_ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/auth/logout", "/user/delete/{id}").hasAnyAuthority("ROLE_NORMUSER", "ROLE_ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/user/update/{id}").hasAnyAuthority("ROLE_NORMUSER", "ROLE_ADMIN")
+                .requestMatchers(HttpMethod.GET, "/user/getById/{id}").hasAnyAuthority("ROLE_NORMUSER", "ROLE_ADMIN")
                 .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
 
