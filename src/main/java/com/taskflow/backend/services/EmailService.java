db0958e0894e85d48c0e36def95c38ed1afcc5a3
DixonGM <127709626+DixonGM@users.noreply.github.com>
2024-10-29 17:27:16 -0600

    update emailService and emailActivate.html
    create emailActivate view

--- src/main/java/com/taskflow/backend/services/EmailService.java
+++ src/main/java/com/taskflow/backend/services/EmailService.java
@@ -28,3 +28,59 @@ public class EmailService {
         mailSender.send(message);
     }
 }
+
+
+
+
+/*
+
+
+//   use mimeMessageHelper porque necesitamos enviar un correo electrónico con contenido HTML. y con simpleMailMessage no se puede enviar contenido HTML.
+
+ package com.taskflow.backend.services;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendActivationEmail(String toEmail, String activationLink) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("taskflowpw2024@gmail.com");
+            helper.setTo(toEmail);
+            helper.setSubject("Activación de tu Cuenta en TaskFlow");
+
+            // Leer el archivo HTML como una cadena
+            String htmlContent = new String(Files.readAllBytes(Paths.get("src/main/resources/templates/activationEmailTemplate.html")));
+
+            // Reemplazar el marcador de enlace de activación con el enlace real
+            htmlContent = htmlContent.replace("{{activationLink}}", activationLink);
+
+            helper.setText(htmlContent, true); // 'true' para indicar que el contenido es HTML
+
+            mailSender.send(message);
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+            // Manejo de errores
+        }
+    }
+}
+    
+
+ */
