74e11d805fff7093067ef86cee9ae30a0078caa7
DeeEmDy <dy-lanarias@hotmail.comcls>
2024-10-29 23:04:57 -0600

    Add SwaggerConfig to Jwt Token Header

--- src/main/java/com/taskflow/backend/TaskflowBackendApplication.java
+++ src/main/java/com/taskflow/backend/TaskflowBackendApplication.java
@@ -3,7 +3,16 @@ package com.taskflow.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "TaskFlow API",
+                version = "1.0", // API version 10/29/2024.
+                description = "TaskFlow API Documentation"
+        )
+)
+
 public class TaskflowBackendApplication {
 
     public static void main(String[] args) {
